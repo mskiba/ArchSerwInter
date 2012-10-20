328a5515487e2c3f3f3f7008d084cc1189cbee47
Marek Skiba <skibamarek@gmail.com>
2012-10-20 14:46:22 +0200

    Wygenerowane rusztowanie aplikacji

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArchSerwInter::Application.config.secret_token = 'b0d658c16b37bab60f95827d352a6c7f49cc6a30737656e391b36259266411efcd37d26ec2f1505d3f3f1ae1d91a0252d92c753a86ad4cab59096c22bd0bc8b1'
