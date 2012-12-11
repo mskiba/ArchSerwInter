62e972588b2c8067786bdc0f50300bc4b5e776fc
Marek Skiba <skibamarek@gmail.com>
2012-12-11 03:58:19 +0100

    Rozbicie daty na dwa inputy

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.8'
 
 gem 'heroku'
+gem 'taps', :git => "git://github.com/ricardochimal/taps.git"
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,22 @@
 class Todo < ActiveRecord::Base
-  attr_accessible :deadline, :opis, :priorytet, :tytul, :screenshot
-  has_attached_file :screenshot, :styles => { :medium => "800x600>" }
+  validates_format_of :deadline_time, :with => /\d{1,2}:\d{2}/
+
+  attr_accessible :deadline, :opis, :priorytet, :tytul, :deadline_date, :deadline_time
+
+  attr_accessor :deadline_date, :deadline_time
+
+	
+  after_initialize :get_datetimes # konwertujemy dla nas
+  before_validation :set_datetimes # konwertujemy dla bazy
+
+  def get_datetimes
+    self.deadline ||= Time.now + (60*60) # defaultowa data, +1h w naszej strefie czasowej
+
+    self.deadline_date ||= self.deadline.to_date.to_s(:db) # yyyy-mm-dd format
+    self.deadline_time ||= "#{'%02d' % self.deadline.hour}:#{'%02d' % self.deadline.min}" # godzina
+  end
+
+  def set_datetimes
+    self.deadline = "#{self.deadline_date} #{self.deadline_time}:00" # wrzucamy date i czas spowrotem do bazy
+  end  
 end
