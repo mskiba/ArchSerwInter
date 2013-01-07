b076b2585a1c83fe3766e61e269e1a4f48dce2d6
Marek Skiba <skibamarek@gmail.com>
2013-01-07 22:33:17 +0100

    Integracja z Heroku ciąg dalszy

--- uruchom_serwer.sh
+++ uruchom_serwer.sh
@@ -5,7 +5,7 @@ export GITHUB_SECRET="6c601c0e26deb036bd63d0adb13525eb53c1d341"
 
 export MONGOID_HOST="localhost"
 export MONGOID_PORT="27017"
-export MONGOID_DATABASE="arch_serw_inter_production"
+export MONGOID_DATABASE="arch_serw_inter_development"
 
 cd bin/
 ./mongod --dbpath=data/db &
